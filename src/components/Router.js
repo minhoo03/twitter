e25fc2940b0bad63b160acd9468fd21442f56ce1
minhoo03 <minhoo0333@gmail.com>
2020-10-20 15:16:42 +0900

    You can get the 'Button name' and use it 'Social Login'

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,37 +1,40 @@
 // app의 요청에 따라 길을 터줌
 
-import React, { useState } from 'react'
+import React from 'react'
+import { Redirect } from 'react-router-dom'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import Auth from '../router/Auth'
 import Home from '../router/Home'
+import Navigation from 'components/Navigation'
+import Profile from 'router/Profile'
 // switch : 하위 라우터 중 하나 고른다
 
 // app.js의 props로 인해 페이지 이동
 const AppRouter = ({isLoggedIn}) => {
     return(
         <Router>
+            {/* isLoggedIn == true == Nav */}
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? 
                 <>
                     <Route exact path='/'>
                         <Home />
                     </Route>
+
+                    <Route exact path='/profile'>
+                        <Profile />
+                    </Route>
                 </>
-                : <Route exact path='/'>
-                    <Auth />
-                </Route> }
+                : 
+                <>
+                    <Route exact path='/'>
+                        <Auth />
+                    </Route> 
+                </>
+                } 
             </Switch>
         </Router>
     )
 }
 export default AppRouter
-
-// npx create-react-app
-// npm install --save firebase
-
-// key 설정 env
-// npm install react-router-dom
-
-// 로그인 / 비로그인 (true 값 따져서) ROUTER 
-
-// Auth 설정으로 로그인 폼
--- src/router/Profile.js
+++ src/router/Profile.js
@@ -1,4 +1,19 @@
+// My Profile : Logout
+import { authService } from 'fbase'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+    const history = useHistory()
+    const onLogOutClick = () => {
+        authService.signOut()
+        history.push('/')
+    }
+    return(
+        <>
+            <button onClick={onLogOutClick}>Logout</button>
+        </>
+    )
+}
+
 export default Profile
