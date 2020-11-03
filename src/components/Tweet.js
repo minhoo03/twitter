864280f3d7af618b9ef549c351e6785e0d9d2c4f
minhoo03 <minhoo0333@gmail.com>
2020-11-03 12:36:28 +0900

    Delete your tweet!!

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,15 +1,30 @@
 import React from "react"
+import { dbService } from 'fbase'
 
-const Tweet = ({tweetObj, isOwner}) => (
-    <div>
-        <h4>{tweetObj.text}</h4>
-        {isOwner && (
-            <>
-            <button>Delete Tweet</button>
-            <button>Edit Tweet</button>
-            </>
-        )}
-    </div>
-)
+const Tweet = ({tweetObj, isOwner}) => {
+
+    
+
+    const onDeleteClick = () => {
+        const ok = window.confirm(`Are you sure you want to delete this tweet?`)
+        console.log(ok)
+        if(ok){
+            // delete tweet
+            dbService.doc(`tweets/${tweetObj.id}`).delete()
+        }
+    }
+
+    return (
+        <div>
+            <h4>{tweetObj.text}</h4>
+            {isOwner && (
+                <>
+                <button onClick={onDeleteClick}>Delete Tweet</button>
+                <button>Edit Tweet</button>
+                </>
+            )}
+        </div>
+    )
+}
 
 export default Tweet
