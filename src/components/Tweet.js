21d1e16d452b2912d3766b72f9d27516376700fb
minhoo03 <minhoo0333@gmail.com>
2020-11-03 13:40:39 +0900

    You can Edit your Tweet!!

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { dbService } from 'fbase'
 
 const Tweet = ({tweetObj, isOwner}) => {
 
+    const [ editing, setEditing ] = useState(false)
+    const [ newTweet, setNewTweet ] = useState(tweetObj.text)
     
-
     const onDeleteClick = () => {
         const ok = window.confirm(`Are you sure you want to delete this tweet?`)
         console.log(ok)
@@ -14,15 +15,45 @@ const Tweet = ({tweetObj, isOwner}) => {
         }
     }
 
+    const toggleEditing = () => setEditing(prev => !prev)
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log(tweetObj, newTweet)
+        dbService.doc(`tweets/${tweetObj.id}`).update({text : newTweet})
+        setEditing(false)
+    }
+
+    const onChange = (event) => {
+        // state 바뀌는 걸 실시간으로
+        const {target : {value}} = event
+        setNewTweet(value)
+    }
+
     return (
         <div>
-            <h4>{tweetObj.text}</h4>
-            {isOwner && (
-                <>
-                <button onClick={onDeleteClick}>Delete Tweet</button>
-                <button>Edit Tweet</button>
-                </>
-            )}
+            {
+                editing ? 
+                ( 
+                    <>
+                        <form onSubmit={onSubmit}>
+                            <input value={newTweet} required onChange={onChange} />
+                            <input type="submit" value="Update Tweet" />
+                        </form> 
+                        <button onClick={toggleEditing}>Cancel</button>
+                    </>
+                ) : (
+                    <>
+                    <h4>{tweetObj.text}</h4>
+                    {isOwner && (
+                        <>
+                        <button onClick={onDeleteClick}>Delete Tweet</button>
+                        <button onClick={toggleEditing}>Edit Tweet</button>
+                        </>
+                    )}
+                    </>
+                ) 
+            }
         </div>
     )
 }
